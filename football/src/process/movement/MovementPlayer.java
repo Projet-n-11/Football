566a9654238e7a3ac01b87d909ec7284ce504934
aladd <aladd@LAPTOP-LRQK3S5L>
2020-02-21 18:11:48 +0100

    process classes

---
+++ football/src/process/movement/MovementPlayer.java
@@ -0,0 +1,11 @@
+package process.movement;
+
+public class MovementPlayer {
+
+	private final int BORDERTOPX = sahut;
+	public int MovementX(int positionX, int borderX) {
+		if(positionX < borderX) {
+			
+		}
+	}
+}
