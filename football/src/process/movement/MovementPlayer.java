ba4958defa47a820b9ec8c4494390c9a9c6bbf04
aladd <aladd@LAPTOP-LRQK3S5L>
2020-02-21 18:14:07 +0100

    process classes

--- football/src/process/movement/MovementPlayer.java
+++ football/src/process/movement/MovementPlayer.java
@@ -2,10 +2,15 @@ package process.movement;
 
 public class MovementPlayer {
 
-	private final int BORDERTOPX = sahut;
+	private final int BORDERTOP = 6;
+	private final int BORDERLEFT = 6;
+	private final int BORDERRIGHT = 78;
+	private final int BORDERBOTTOM = 42;
+	
 	public int MovementX(int positionX, int borderX) {
 		if(positionX < borderX) {
 			
 		}
+		return 0;
 	}
 }
