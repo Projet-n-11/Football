06e2bef043e86096d5fee26065c77c9b3a9ed40e
aladd <aladd@LAPTOP-LRQK3S5L>
2020-03-11 15:44:27 +0100

    New data class for neutral elements

---
+++ football/src/datafield/Grass.java
@@ -0,0 +1,10 @@
+package datafield;
+
+public class Grass extends AbstractPosition{
+	
+	//This is just a neutral element used when there's nothing
+	//onto the field
+	public Grass(int positionX, int positionY) {
+		super(positionX, positionY);
+	}
+}
