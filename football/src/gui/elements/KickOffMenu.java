a9e710676f1012188d2fd27edc4e53e6b064bb7d
quitt <quitt@192.168.1.110>
2020-03-17 19:52:04 +0100

    Edit: GraphicalPanel Create : KickOffMenu, MainMenu

---
+++ football/src/gui/elements/KickOffMenu.java
@@ -0,0 +1,166 @@
+package gui.elements;
+
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import dataplayer.DataPlayer;
+import datateam.DataTeam;
+import process.management.CreaTeam;
+import process.management.RecupTeam;
+
+public class KickOffMenu extends JFrame{
+	private JFrame mainFrame;
+	
+	public KickOffMenu() {
+		this("Configuration");
+	}
+	
+	public KickOffMenu(String title) {
+		mainFrame=new JFrame();
+		mainFrame.getContentPane().setLayout(new GridLayout(4,1));
+		
+		
+		JLabel choice=new JLabel("Choose your team");
+		
+		mainFrame.getContentPane().add(choice);
+		mainFrame.getContentPane().add(new JLabel(""));
+		
+		GridLayout grid1=new GridLayout(1,2);
+		GridLayout grid2=new GridLayout(1,2);
+		
+				
+		try {
+			System.out.println("truc de merde");
+			ComboBoxModel modelMere = choosingTeams();
+			ComboBoxModel modelTFrance = playersTitularTeam("France");
+			ComboBoxModel modelTBrazil = playersTitularTeam("Brazil");
+			ComboBoxModel modelSFrance= playersSubstituteTeam("France");
+			ComboBoxModel modelSBrazil = playersSubstituteTeam("Brazil");
+			
+			JComboBox mere=new JComboBox(modelMere);
+			JComboBox filleT =new JComboBox();
+			JComboBox filleS= new JComboBox();
+			mere.addItemListener(new ItemListener(){
+				public void itemStateChanged(ItemEvent e) {
+					
+					if(mere.getSelectedItem().equals("France")) {
+						
+						filleT.setModel(modelTFrance);
+						filleS.setModel(modelSFrance);
+						
+					}
+					else if(mere.getSelectedItem().equals("Brazil")) {
+						
+						filleT.setModel(modelTBrazil);
+						filleS.setModel(modelSBrazil);
+						
+					}
+				} 
+			});
+			
+			mainFrame.getContentPane().add(mere,grid1);
+			mainFrame.getContentPane().add(new JLabel(""),grid1);
+			mainFrame.getContentPane().add(filleT,grid2);
+			mainFrame.getContentPane().add(filleS,grid2);
+			initJFrame(mainFrame);
+		}
+		catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.err.println("erreur de merde");
+		}
+			
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new KickOffMenu();
+		System.out.println("fait");
+	
+	}
+	
+	private void initJFrame(JFrame frame) {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//this.pack();
+		frame.setPreferredSize(null);
+		frame.setVisible(true);
+	}
+	private DefaultComboBoxModel choosingTeams() throws IOException {
+		
+		DefaultComboBoxModel equipe;
+		String [] tabName = new String[50];
+		ArrayList<String> nameteam= RecupTeam.getCountriesNames();
+		int i=0;
+		for(Iterator<String> it= nameteam.iterator(); it.hasNext();) {
+			tabName[i]=it.next();
+			i++;
+		}
+	
+	
+		equipe=new DefaultComboBoxModel(tabName);
+
+		return equipe;
+	}
+	private DefaultComboBoxModel playersSubstituteTeam(String teamName) throws IOException {
+		DefaultComboBoxModel substitute;
+		String [] tabPlayer=new String[23];
+		DataTeam team= CreaTeam.creaTeam(teamName);
+		HashMap<String,DataPlayer> hm=team.getPlayers();
+		
+		ArrayList<DataPlayer> values=new ArrayList<>(hm.values());
+		int i=0;
+		
+		for(DataPlayer dp : values) {
+			if(dp.getPlayerType().getTitularPlayer()==0) {
+				tabPlayer[i]= dp.getPlayerName()+ " : " + dp.getPlayerType().getPlayerTypeName();
+				i++;
+			}
+		}
+		substitute=new DefaultComboBoxModel(tabPlayer);
+		
+		return substitute;
+	}
+	private DefaultComboBoxModel playersTitularTeam(String teamName) throws IOException {
+		DefaultComboBoxModel players;
+		String [] tabPlayer=new String[23];
+		DataTeam team= CreaTeam.creaTeam(teamName);
+		HashMap<String,DataPlayer> hm=team.getPlayers();
+		
+		ArrayList<DataPlayer> values=new ArrayList<>(hm.values());
+		int i=0;
+		
+		for(DataPlayer dp : values) {
+			if(dp.getPlayerType().getTitularPlayer()==1) {
+				tabPlayer[i]= dp.getPlayerName()+ " : " + dp.getPlayerType().getPlayerTypeName();
+				i++;
+			}
+		}
+		players=new DefaultComboBoxModel(tabPlayer);
+		
+		return players;
+		
+	}
+}	
+	
+
+	
---
+++ football/src/gui/elements/MainMenu.java
@@ -0,0 +1,159 @@
+package gui.elements;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+public class MainMenu extends JFrame {
+	private JFrame mainFrame;
+	
+	private JLabel simulator;
+	private JButton kickOff;
+	private JButton options;
+	private JButton credits;
+	
+	private JButton leave;
+	
+	public MainMenu() {
+		this("Fooball Game");
+	}
+	
+	public MainMenu(String title) {
+		mainFrame=new JFrame();
+		mainFrame.getContentPane().setLayout(new GridLayout(6,1));
+		mainFrame.setSize(1000,500);
+		
+		JPanel jp1=new JPanel();
+		JPanel jp2=new JPanel();
+		JPanel jp3=new JPanel();
+		JPanel jp4=new JPanel();
+		JPanel jp5=new JPanel();
+	
+		jp1.setLayout(new FlowLayout(FlowLayout.CENTER));
+		jp2.setLayout(new FlowLayout(FlowLayout.CENTER));
+		jp3.setLayout(new FlowLayout(FlowLayout.CENTER));
+		jp4.setLayout(new FlowLayout(FlowLayout.CENTER));
+		jp5.setLayout(new FlowLayout(FlowLayout.CENTER));
+	
+		simulator=new JLabel("FOOTBALL SIMULATOR");
+		simulator.setForeground(Color.RED);
+		jp1.add(simulator);
+		
+		kickOff=new JButton("Kick-off");
+		kickOff.setMinimumSize(getSize());
+		
+		kickOff.addActionListener(new ActionKickOff());
+		jp2.add(kickOff);
+		
+		options =new JButton("Options");
+		options.setSize(200,400);
+		options.addActionListener(new ActionOptions());
+		jp3.add(options);
+		
+		credits=new JButton("Credits");
+		credits.setSize(200,400);
+		credits.addActionListener(new ActionCredits());
+		jp4.add(credits);
+		
+		leave=new JButton("Quit the game");
+		leave.addActionListener(new ActionLeave());
+		leave.setSize(200,400);
+		jp5.add(leave);
+		
+		mainFrame.getContentPane().add(jp1);
+		mainFrame.getContentPane().add(jp2);
+		mainFrame.getContentPane().add(jp3);
+		mainFrame.getContentPane().add(jp4);
+		mainFrame.getContentPane().add(jp5);
+
+		initJFrame(mainFrame);
+		
+	}
+	
+	public static void main(String[] args) {
+		new GraphicalPanel();
+	
+	}
+	/*public void paint(Graphics g)
+    {
+        try
+        {
+            Image img=ImageIO.read(new File ("\ressources\icone.jpeg"));
+            g.drawImage(img, 0, 0, 1440, 900, this);
+            ImageLoader Image=new ImageLoader();
+            ImageLoader Image2=new ImageLoader();
+            this.add(Image);
+            this.add(Image2);
+        }
+        catch (Exception e )
+        {
+             
+        }
+    }*/
+
+	public class ActionKickOff implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			mainFrame.getContentPane().removeAll();
+			mainFrame.repaint();
+			
+		}
+		
+	}
+	
+	public class ActionOptions implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	
+	public class ActionCredits implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			
+			String message="Produced by : \n ALADDINE BEN ROMDHANE \n LAURA FUSTINONI \n QUITTERIE PILON";
+			JOptionPane optionPane=new JOptionPane();
+			optionPane.showMessageDialog(null, message, "Credits", JOptionPane.INFORMATION_MESSAGE);
+			
+		}
+	}
+	
+	public class ActionLeave implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane optionPane =new JOptionPane();
+			
+			int op=optionPane.showConfirmDialog(null, "You sure about that?", "Quit",JOptionPane.YES_NO_OPTION );
+			if(op==0) {
+				System.exit(0);
+			}
+		}
+		
+	}
+	
+	
+	private void initJFrame(JFrame frame) {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//this.pack();
+		frame.setPreferredSize(null);
+		frame.setVisible(true);
+	}
+	
+	public JFrame getJFrameMainMenu() {
+		return mainFrame;
+	}
+}
+
+
