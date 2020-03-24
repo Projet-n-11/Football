3232f3efbb985389f79d3ab441dbe1da01d47bc6
laura <laura@LAPTOP-14VC5ACT>
2020-03-24 18:14:43 +0100

    edit match and movement player

--- football/src/process/management/Match.java
+++ football/src/process/management/Match.java
@@ -5,12 +5,13 @@ import java.util.Iterator;
 
 import datafield.Grass;
 import datafield.Position;
+import datafield.SpecialPosition;
 import dataplayer.DataPlayer;
 import datateam.DataTeam;
 import process.movement.Vision;
 import process.movement.MovementPlayer;
 
-public class Match {
+public class Match {	// if singleton : re-chech every variables
 	
 	
 	public static Boolean goal, outOfField, falt;
@@ -21,7 +22,7 @@ public class Match {
 	 * @param userTeam
 	 * @param botTeam
 	 */
-	public static void match_1_Big_round(DataTeam userTeam, DataTeam botTeam) { 		
+	public static void matchOneRound(DataTeam userTeam, DataTeam botTeam) { 		
 		
 		Iterator<DataPlayer> itUser;
 		Iterator<DataPlayer> itBot;
@@ -30,6 +31,7 @@ public class Match {
 		Boolean bothHavePlayed;
 		Map positions = new Map();
 		MovementPlayer mp = new MovementPlayer();
+		SpecialPosition specPos = new SpecialPosition();
 		int i;
 		
 		//each round we initialize the list (iterator) of players to check
@@ -43,11 +45,13 @@ public class Match {
 		while ((!goal || !outOfField || !falt) && (itUser.hasNext() || itBot.hasNext()) ){
 			
 				bothHavePlayed = false;
+				Position posBall = null;	// singleton ?
 				
 				while (!bothHavePlayed) {
-					if(itsUserRound && itUser.hasNext()) { //careful : the end of the list may be reached,
-						currentPlayer = itUser.next();		// this is why we use hasNext()
+					if(itsUserRound && itUser.hasNext()) { //careful, but the end of the list may not be reached
+						currentPlayer = itUser.next();
 						itsUserRound = false;
+						bothHavePlayed = false;
 					}
 					else {
 						currentPlayer = itBot.next();
@@ -66,20 +70,27 @@ public class Match {
 						 * if it is not, player have to run to the ball
 						 */
 						if (objectsSeen.get(i).getClass().getName().contentEquals("databall.DataBall")==true) {
+							
+							posBall = objectsSeen.get(i);
+							
 							/**
 							 * if the player owns the ball, he has to run to the goal.
 							 */
-							if (Vision.areClose(objectsSeen.get(i),currentPlayer)) {
-								// create methode moveToGoal : replace player ET ball au bon endroit
+							if (Vision.areClose(posBall,currentPlayer)) {
+								mp.runWithBall(posBall,currentPlayer, itsUserRound); // replace player AND ball to new destination
 							}
-							
+							else {
 							Grass grass = new Grass(currentPlayer.getPositionX(),currentPlayer.getPositionY());
-					 		mp.move(objectsSeen.get(i),currentPlayer); //should be static, modify only the attributes positionning data
+					 		mp.move(posBall,currentPlayer); //should be static, modify only the attributes positionning data
 							positions.setElement(grass);
 							positions.setElement(currentPlayer);
+							}
 						}
 					}
 					
+					/**
+					 * to this later
+					 */
 					if (currentPlayer.getPlayerType().getPlayerTypeName().compareTo("Gardien")!=0) {
 						/**
 						 * this loop is about all players that AREN'T GOALIES.
@@ -89,7 +100,11 @@ public class Match {
 					
 				}
 				
-				// vérifier la situation : ballon sorti ? Joueurs en faute ?
+				// one test possible but the goal should not be considered as one square but an area
+				if (posBall.getPositionX()==specPos.getGoal1().getPositionX() && posBall.getPositionY()==specPos.getGoal1().getPositionY()) 
+				{
+					goal = true;
+				}
 		}
 	}
 	
--- football/src/process/movement/MovementPlayer.java
+++ football/src/process/movement/MovementPlayer.java
@@ -2,6 +2,7 @@ package process.movement;
 
 import databall.DataBall;
 import datafield.Position;
+import datafield.SpecialPosition;
 import dataplayer.DataPlayer;
 
 
@@ -17,13 +18,14 @@ public class MovementPlayer {
 	private final int BORDERBOTTOM = 48;
 	private final int BORDERLEFT = 0;
 	private final int BORDERRIGHT = 84;
+	private SpecialPosition specPos;
 	//private int movX = 0;
 	//private int movY = 0;
 	
 	
 	
 	public MovementPlayer() {
-
+		specPos = new SpecialPosition();
 	}
 	
 	
@@ -102,29 +104,30 @@ public class MovementPlayer {
 					//////////////////////////////////:
 				}	
 				System.out.println("Coordinates : x = " + dp.getPositionX() + " ; y = " + dp.getPositionY());
-				
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
 		}
 	}
 
-	public void runWithBall(DataBall ball, DataPlayer player) {
+	public void runWithBall(Position ball, DataPlayer player, boolean itsUserRound) {
 		int d= 0;
-		/* 
-		stock position of player
+		Position goal;
+		if (itsUserRound) // in order to know which goal to go...
+		{
+			goal = specPos.getGoal2();
+		}
+		else goal = specPos.getGoal1();
+		 
+		int x = player.getPositionX();
+		int y = player.getPositionY(); //stock old position to know direction and then which side place ball
+		
 		move(goal, player);
-		if ( ( stock.positionX() - player.positionY() )>=0 ) 
+		if ( ( y - player.getPositionY() )>=0 ) // d is to place ball on the good side of player
 		{
 			d = 1;
 		}
 		else 
 		{
 			d = -1;
-		}
-		*/ 
+		} 
 		ball.setPositionX(player.getPositionX());
 		ball.setPositionY(player.getPositionY()+d);
 	}
