4c40b5133d78d27c9798b9cc3cef4ad6d57f82df
aladd <aladd@Aladdine-PREDATOR>
2020-04-10 21:12:55 +0200

    Changing TestFootballSimulation

--- football/src/test/TestFootballSimulation.java
+++ football/src/test/TestFootballSimulation.java
@@ -1,21 +1,11 @@
 package test;
 
-import java.io.IOException;
-
-import databall.DataBall;
-import datateam.DataTeam;
 import gui.elements.MainFrame;
-import gui.elements.MatchScreen;
-import process.management.ConstantPosition;
-import process.management.CreaTeam;
-import process.management.PositionTactics;
-import process.movement.MovementBall;
-import process.management.Map;
-import process.management.PositionBall;
 
 /*
- * This class is used to test if the Positioning class works
+ * This class will be the one who will launch the game
  */
+
 public class TestFootballSimulation {
 	
 	public static void main(String[] args){
